d8133687d08542dd852bd40d8b1f040425c49903
Ed <ed.churchill00@gmail.com>
2021-11-30 14:35:09 +0000

    Create tube edges graph

--- test.py
+++ test.py
@@ -1,22 +1,38 @@
 import pydotplus as pdp
+import os
+import ast
 
- # Create test graph
-test_graph = pdp.graphviz.Graph(graph_name='test_graph',
+from pydotplus.graphviz import Node
+
+ # Create London graph
+london_graph = pdp.graphviz.Graph(graph_name='london_graph',
                                         graph_type='graph',
                                         simplify=True)
 
-# Create test nodes
-test_node_one = pdp.graphviz.Node(name='1')
-test_node_two = pdp.graphviz.Node(name='2')
+# Create graph nodes
+for i in range(1, 200):
+    node_i = pdp.graphviz.Node(name=str(i))
+    london_graph.add_node(node_i)
 
-# Create test edges
-test_edge = pdp.graphviz.Edge(src='1', dst='2')
+#######---------------------------------------------------
+# Create graph edges, using the edges stored in text files
+#######---------------------------------------------------
 
-# Add nodes and edges to graph
-test_graph.add_node(test_node_one)
-test_graph.add_node(test_node_two)
-test_graph.add_edge(test_edge)
+#  Read tube edges from text file
+with open('tube_edges.txt') as f:
+    # Read edges from text file and convert to list
+    tube_edges = f.readline()
+    
+# Add tube edges to graph
+tube_edges = ast.literal_eval(tube_edges)
+for edge in tube_edges:
+    edge = pdp.graphviz.Edge(src=str(edge[0]), dst=str(edge[1]))
+    london_graph.add_edge(edge)
 
-# Write graph to .gv file
-with open('test_graph.gv', 'w') as f:
-    f.write(test_graph.to_string())
+# Write tube graph to .gv file
+with open('tube_edges.gv', 'w') as f:
+    f.write(london_graph.to_string())
+
+# Write tube graph to .png file
+os.system('dot -Tpng tube_edges.gv -o london_graph.png')
+
