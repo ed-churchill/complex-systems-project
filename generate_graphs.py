119dc3a265949e39df225fa183ee84852dc222c0
Ed <ed.churchill00@gmail.com>
2021-12-20 21:56:12 +0000

    Add functions to add nodes and edges to graph

--- generate_graphs.py
+++ generate_graphs.py
@@ -3,23 +3,21 @@ import os
 import ast
 from players import Detective, MisterX
 
-
-def generate_initial_graph():
-    """Function that returns a pdp.graphviz.Graph object of the initial graph of London, with 
-    Mister X and the detectives at their initial locations"""
-
-    # Initialise graph
-    london_graph = pdp.graphviz.Graph(graph_name='london_graph',
-                                            graph_type='graph',
-                                            simplify=False)
+def add_nodes(graph, mister_x_location, detective_locations):
+    """Adds nodes to the given graph, taking into account the provided location of mister_x
+    (an int between 1 and 200), as well as the location of the detectives (provided as a list of 3 
+    integers, each between 1 and 200)"""
 
     # Add graph nodes
     for i in range(1, 200):
         node_i = pdp.graphviz.Node(name=str(i))
         node_i.set('penwidth', 3)
         node_i.set('fontsize', 40)
-        london_graph.add_node(node_i)
-        
+        graph.add_node(node_i)
+
+def add_edges(graph):
+    """Adds edges to the graph of London"""
+
     # Read edges from text file
     with open('Edges/tube_edges.txt') as f:
         tube_edges = f.readline()
@@ -33,22 +31,36 @@ def generate_initial_graph():
     bus_edges = ast.literal_eval(bus_edges)
     taxi_edges = ast.literal_eval(taxi_edges)
 
-    # Add edges
+    # Add edges to graph
     for edge in tube_edges:
         edge = pdp.graphviz.Edge(src=str(edge[0]), dst=str(edge[1]))
         edge.set('color', 'red')
         edge.set('penwidth', 4)
-        london_graph.add_edge(edge)
+        graph.add_edge(edge)
     for edge in bus_edges:
         edge = pdp.graphviz.Edge(src=str(edge[0]), dst=str(edge[1]))
         edge.set('color', 'blue')
         edge.set('penwidth', 4)
-        london_graph.add_edge(edge)
+        graph.add_edge(edge)
     for edge in taxi_edges:
         edge = pdp.graphviz.Edge(src=str(edge[0]), dst=str(edge[1]))
         edge.set('color', 'green')
         edge.set('penwidth', 4)
-        london_graph.add_edge(edge)
+        graph.add_edge(edge)
+
+
+def generate_initial_graph():
+    """Function that returns a pdp.graphviz.Graph object of the initial graph of London, with 
+    Mister X and the detectives at their initial locations"""
+
+    # Initialise graph
+    london_graph = pdp.graphviz.Graph(graph_name='london_graph',
+                                            graph_type='graph',
+                                            simplify=False)
+
+    # Add nodes and edges
+    add_nodes(london_graph, mister_x_location=45, detective_locations=[1, 2, 3])
+    add_edges(london_graph)
 
     return london_graph
 
@@ -62,3 +74,6 @@ def draw_graph(graph, graph_name):
 
     # Write graph to .png file, using the already created .gv file
     os.system(f'dot -Tpng {graph_name}.gv -o {graph_name}.png')
+
+london_graph = generate_initial_graph()
+draw_graph(london_graph, 'london_graph')
