8ea9c57a01174b8f6997647e65c79a7a3365bc5f
Ed <ed.churchill00@gmail.com>
2021-12-20 21:06:00 +0000

    Create MisterX class and detective Class

---
+++ players.py
@@ -0,0 +1,14 @@
+class MisterX:
+    """A class to model Mister X"""
+
+    # Constructor, where 'initial_location' is an integer between 1 and 200
+    def __init__(self, initial_location):
+        self.location = initial_location
+
+class Detective:
+    """A class to model a detective"""
+
+    # Constructor, where 'initial_location' is an integer between 1 and 200
+    def __init__(self, initial_location):
+        self.location = initial_location
+
