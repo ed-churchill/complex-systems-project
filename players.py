c9ebd73c8c5fa5c1c58e9d41c36ee3010302491c
Ed <ed.churchill00@gmail.com>
2021-12-20 21:35:51 +0000

    Add if statement in constructor to check if initial location is invalid

--- players.py
+++ players.py
@@ -3,12 +3,21 @@ class MisterX:
 
     # Constructor, where 'initial_location' is an integer between 1 and 200
     def __init__(self, initial_location):
-        self.location = initial_location
+        # First check if initial location is valid
+        if initial_location > 200 or initial_location < 1:
+            raise Exception("Invalid initial location.")
+        else:
+            self.location = initial_location
 
 class Detective:
     """A class to model a detective"""
 
     # Constructor, where 'initial_location' is an integer between 1 and 200
     def __init__(self, initial_location):
-        self.location = initial_location
+        # First check if initial location is valid
+        if initial_location > 200 or initial_location < 1: 
+            raise Exception("Invalid initial location.")
+        else:
+            self.location = initial_location
+    
 
