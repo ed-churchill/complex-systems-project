ab93adda5275cb94175729a7cf58179021d16329
Ed <ed.churchill00@gmail.com>
2021-12-21 11:09:08 +0000

    Add initial vertices

--- players.py
+++ players.py
@@ -1,23 +1,29 @@
+import random
+
 class MisterX:
     """A class to model Mister X"""
 
-    # Constructor, where 'initial_location' is an integer between 1 and 200
-    def __init__(self, initial_location):
-        # First check if initial location is valid
-        if initial_location > 200 or initial_location < 1:
-            raise Exception("Invalid initial location.")
-        else:
-            self.location = initial_location
+    def __init__(self):
+        """Constructor, assigning MisterX an initial location"""
+        
+        # Choose a random starting location for MisterX from the possible starting locations
+        initial_locations = [45, 51, 71, 78, 104, 106, 127, 132, 146, 166, 170, 172]
+        initial_location = random.choice(initial_locations)
+
+        # Initialise location using initial location
+        self.location = initial_location
 
 class Detective:
     """A class to model a detective"""
 
-    # Constructor, where 'initial_location' is an integer between 1 and 200
-    def __init__(self, initial_location):
-        # First check if initial location is valid
-        if initial_location > 200 or initial_location < 1: 
-            raise Exception("Invalid initial location.")
-        else:
-            self.location = initial_location
+    def __init__(self):
+        """Constructor, assigning a detective an initial location"""
+        
+        # Choose a random starting location for MisterX from the possible starting locations
+        initial_locations = [13, 26, 29, 34, 50, 53, 91, 94, 103, 112, 117, 138, 141, 155, 174]
+        initial_location = random.choice(initial_locations)
+
+        # Initialise location using initial location
+        self.location = initial_location
     
 
