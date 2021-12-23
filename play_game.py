eb93e224ea51124e5af9c20ae1c2dd281dfdfa1a
Ed <ed.churchill00@gmail.com>
2021-12-23 18:24:03 +0000

    Split play_random_game into sub functions for mister x move and detectives move

--- play_game.py
+++ play_game.py
@@ -37,42 +37,15 @@ def play_random_game(mister_x, detectives):
         current_graph = generate_graph(mister_x.location, [detective.location for detective in detectives])
         draw_graph(current_graph, f'graph_{k}')
 
-        # Calculate Mister X's possible moves
-        misterx_moves = mister_x.possible_moves(tube_edges, bus_edges, taxi_edges)
-        for detective in detectives:
-            if detective.location in misterx_moves:
-                misterx_moves.remove(detective.location)
-        
-        # If list of possible moves is empty, then detectives win
-        if not misterx_moves:
-            end_graph = generate_graph(mister_x.location, [detective.location for detective in detectives])
-            draw_graph(end_graph, 'end_graph')
-            print("Game over. Detectives win")
+        # Carry out mister X's move and check if game has terminated. If it hasn't, update Mister X to
+        # latest version
+        mister_x = misterx_random_turn(mister_x, detectives, tube_edges, bus_edges, taxi_edges)
+        if mister_x == 1:
+            print('Game over. Detectives win.')
             return 1
-        else:
-            # Otherwise, move to a random node out of the possible nodes
-            mister_x.location = random.choice(misterx_moves)
 
-        # Calculate the detectives' possible moves.
-        for i, detective in enumerate(detectives):
-            # Ensure no clashes with other detectives
-            detective_moves = detective.possible_moves(tube_edges, bus_edges, taxi_edges)
-            if detectives[i - 1].location in detective_moves:
-                detective_moves.remove(detectives[i - 1].location)
-            if detectives[i - 2].location in detective_moves:
-                detective_moves.remove(detectives[i - 2].location)
-            if detectives[i - 3].location in detective_moves:
-                detective_moves.remove(detectives[i - 3].location)
-
-            # If list of possible moves is empty, then MisterX wins
-            if not detective_moves:
-                end_graph = generate_graph(mister_x.location, [detective.location for detective in detectives])
-                draw_graph(end_graph, 'end_graph')
-                print("Game over. Mister X wins.")
-                return 0
-            else:
-                # Otherwise, move to a random node out of the possible nodes
-                detective.location = random.choice(detective_moves)
+        # Carry out the detectives' move
+        detectives = detectives_random_move(detectives, tube_edges, bus_edges, taxi_edges)
 
         # Check if any of the detectives have the same location as Mister X
         detective_locations = [detective.location for detective in detectives]
@@ -87,6 +60,50 @@ def play_random_game(mister_x, detectives):
     draw_graph(end_graph, 'end_graph')
     print("Game over. Mister X wins.")
     return 0
+
+def misterx_random_turn(mister_x, detectives, tube_edges, bus_edges, taxi_edges):
+    """Function that carries out Mister X's turn using the random walk strategy. The 
+    function returns 1 if Mister X has no possible moves and the detectives win, otherwise
+    it return the current instance of Mister X, to be used as a parameter in the
+    'detective_random_turn' function."""
+
+    # Calculate Mister X's possible moves
+    misterx_moves = mister_x.possible_moves(tube_edges, bus_edges, taxi_edges)
+    for detective in detectives:
+        if detective.location in misterx_moves:
+            misterx_moves.remove(detective.location)
+     
+    # If list of possible moves is empty, then detectives win
+    if not misterx_moves:
+        end_graph = generate_graph(mister_x.location, [detective.location for detective in detectives])
+        draw_graph(end_graph, 'end_graph')
+        return 1
+    else:
+        # Otherwise, move to a random node out of the possible nodes
+        mister_x.location = random.choice(misterx_moves)
+        return mister_x
+
+def detectives_random_move(detectives, tube_edges, bus_edges, taxi_edges):
+    """Function that carries out the detectives' turn using the random walk strategy. The 
+    function returns 0 if any detective has no possible moves and Mister X wins, otherwise
+    it return the current instances of the detectives as a list , to be used as a parameter in the
+    'detective_random_turn' function."""
+
+    # Calculate the detectives' possible moves.
+    for i, detective in enumerate(detectives):
+        # Ensure no clashes with other detectives
+        detective_moves = detective.possible_moves(tube_edges, bus_edges, taxi_edges)
+        if detectives[i - 1].location in detective_moves:
+            detective_moves.remove(detectives[i - 1].location)
+        if detectives[i - 2].location in detective_moves:
+            detective_moves.remove(detectives[i - 2].location)
+        if detectives[i - 3].location in detective_moves:
+            detective_moves.remove(detectives[i - 3].location)
+
+        # Move to a random node out of the possible nodes
+        detective.location = random.choice(detective_moves)
+
+    return detectives    
 
 if __name__ == "__main__":
     mister_x, detectives = initialise_game()
